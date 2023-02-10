8881b6d21dc964f72a0c74b66a84fff69d17259b
Carl Sustrich <carl.sustrich@gmail.com>
2023-02-10 12:05:11 -0800

    pass test 2

--- __tests__/conversionLogic.js
+++ __tests__/conversionLogic.js
@@ -7,7 +7,14 @@ describe('conversionLogic', () => {
     let toCurrencyRate = 3.6725;
     let value = 15;
     let finalValue = conversionLogic(value, fromCurrencyRate, toCurrencyRate);
-    expect(finalValue).toEqual(55.09)
+    expect(finalValue).toEqual(55.09);
   });
 
+  test('it should take a value not in USD and convert it to the desired currency, ALL to BOB', () => {
+    let fromCurrencyRate = 108.19408970;
+    let toCurrencyRate = 6.92230329;
+    let value = 15;
+    let finalValue = conversionLogic(value, fromCurrencyRate, toCurrencyRate);
+    expect(finalValue).toEqual(.96);
+  });
 });
--- src/conversionLogic.js
+++ src/conversionLogic.js
@@ -1,3 +1,5 @@
 export function conversionLogic(value, fromCurrencyRate, toCurrencyRate) {
-  return Number(((value*fromCurrencyRate)*toCurrencyRate).toFixed(2));
+  let finalValue;
+  fromCurrencyRate === 1 ? finalValue = Number((value*toCurrencyRate).toFixed(2)) : finalValue = Number(((value/fromCurrencyRate)*toCurrencyRate).toFixed(2))
+  return finalValue;
 }
